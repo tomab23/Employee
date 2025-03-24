b4c66369b3d21629bc7bffcf4d99468cadd0db42
thomas bartier <thomas.bartier@gmail.com>
2025-03-24 15:50:15 +0100

    test env netlify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     <div>
         <h1>HomePage</h1>
 
-        <h2 className="mt-10">{import.meta.env.VITE_SUPABASE_KEY}</h2>
+        <h2 className="mt-10">key test : {import.meta.env.VITE_SUPABASE_KEY}</h2>
     </div>
   )
 }
