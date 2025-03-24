5cad2261ed3c20a086230a68bdd988167efb1456
thomas bartier <thomas.bartier@gmail.com>
2025-03-24 15:46:27 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [tailwindcss(), react()],
           // server: {
       //   host: ''
-      // },
-      define: {
-        'process.env.VITE_SUPABASE_KEY':JSON.stringify(process.env.VITE_SUPABASE_KEY)
-      }
+      // }
 });
