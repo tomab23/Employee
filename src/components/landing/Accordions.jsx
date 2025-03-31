915dfe05883443037cec81b582b287316900da7b
thomas bartier <thomas.bartier@gmail.com>
2025-03-31 14:50:02 +0200

    fix accordion + add title img contact

--- src/components/landing/Accordions.jsx
+++ src/components/landing/Accordions.jsx
@@ -4,27 +4,27 @@ const Accordions = () => {
   return (
     <div className="join join-vertical bg-base-100">
     <div className="collapse collapse-arrow join-item border-base-300 border">
-      <input type="radio" name="my-accordion-4" />
+      <input type="radio" name="my-accordion-4" defaultChecked />
       <div className="collapse-title font-semibold">Pourquoi avoir fais le site ?</div>
       <div className="collapse-content text-sm">J'ai fait le site pour m'entraîner et tester Supabase.</div>
     </div>
     <div className="collapse collapse-arrow join-item border-base-300 border">
-      <input type="radio" name="my-accordion-4" />
+      <input type="radio" name="my-accordion-4"  />
       <div className="collapse-title font-semibold">Quelles technologies sont utiliser pour le site ?</div>
       <div className="collapse-content text-sm">J'utilise React avec vite, Tailwind, React-router, Lucide pour les icons, I18next pour la traduction et Supabase pour le backend.</div>
     </div>
     <div className="collapse collapse-arrow join-item border-base-300 border">
-      <input type="radio" name="my-accordion-4" defaultChecked />
+      <input type="radio" name="my-accordion-4"  />
       <div className="collapse-title font-semibold">Pourquoi utiliser le site ?</div>
       <div className="collapse-content text-sm">Pour gerer vos employés de votre entreprise ou de vos entreprises.</div>
     </div>
     <div className="collapse collapse-arrow join-item border-base-300 border">
-      <input type="radio" name="my-accordion-4" defaultChecked />
+      <input type="radio" name="my-accordion-4"  />
       <div className="collapse-title font-semibold">Puis-je changer de thème ?</div>
       <div className="collapse-content text-sm">Vous avez le choix entre différents thème (se trouvent tout en bas de la page) mais l'application est basé sur le thème par default.</div>
     </div>
     <div className="collapse collapse-arrow join-item border-base-300 border">
-      <input type="radio" name="my-accordion-4" defaultChecked />
+      <input type="radio" name="my-accordion-4"  />
       <div className="collapse-title font-semibold">L'application est-elle traduite ?</div>
       <div className="collapse-content text-sm">L'application est traduite en français et en anglais, vous pouvez changer la langue en bas de page, à gauche du changement de thème.</div>
     </div>
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,7 +13,7 @@ const ContactPage = () => {
       </div>
       <div className="min-h-[90vh] mx-2 flex max-md:flex-col-reverse justify-center max-md:justify-end items-center gap-15 2xl:gap-40  max-xl:gap-10 max-md:gap-5">
         <ContactForm />
-        <img src={img} alt="" className="w-120 max-xl:w-100 max-md:w-80" />
+        <img src={img} alt="" className="w-120 max-xl:w-100 max-md:w-80" title='illustrations by Storyset' />
       </div>
       <Footer fixed />
     </div>
